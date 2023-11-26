a8905fd796db70b3915df5beb4006748b74cc3d4
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-11-26 12:28:24 -0800

    correct(

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """Render"""
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+
+    return render_template(path, states=states)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Clean-up"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
